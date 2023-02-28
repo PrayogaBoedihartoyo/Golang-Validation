bd1e37f0333464f7b36369c6c59f9331d3e950bb
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-02-28 15:21:04 +0700

    tag_parameter

--- multiple_validation_test.go
+++ multiple_validation_test.go
@@ -15,3 +15,13 @@ func TestMultipleValidation(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestTagParamenter(t *testing.T) {
+	validate := validator.New()
+	var user string = "123"
+
+	err := validate.Var(user, "required,number,min=3,max=5")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
